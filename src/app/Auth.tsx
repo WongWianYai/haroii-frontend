3884a0df8c233f0eb165947f98812da16f84ee7d
Chavaponsuu <chavapon1402@gmail.com>
2025-09-05 02:10:07 +0700

    change text all field to thai and add more field for register

--- src/app/Auth.tsx
+++ src/app/Auth.tsx
@@ -3,66 +3,67 @@ import React, { ChangeEvent, FormEvent, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
-import { API_URL } from '@/config';
+import { API_URL } from "@/config";
+import { Eye, EyeOff } from "lucide-react";
 
 import Link from "next/link";
 
 interface FormData {
+  name: string;
+  phone: string;
   email: string;
   password: string;
-  confirmPassword: string;
 }
 
-function SignUp() {
+function Auth() {
   const [formData, setFormData] = useState<FormData>({
+    name: "",
+    phone: "",
     email: "",
     password: "",
-    confirmPassword: "",
   });
   const [isRegister, setIsRegister] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  
+  const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit =  async (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!isRegister &&formData.password !== formData.confirmPassword) {
-      alert("Passwords do not match!");
-      return;
-    }
+
     setLoading(true);
     setError("");
     try {
-    const res = await fetch(`${API_URL}/auth/${isRegister ? 'login' : 'register-restaurant'}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // Add CORS headers if needed
-      },
-      credentials: 'include', // Important for cookies
-      body: JSON.stringify({
-        email: formData.email,
-        password: formData.password,
-        confirmPassword: formData.confirmPassword
-      })
-    });
-    if(!res.ok){
-      const err = await res.json();
-      throw new Error(err.message || (!isRegister ? "Sign up failed" : "Login failed"));
+      const res = await fetch(
+        `${API_URL}/auth/${isRegister ? "login" : "register-restaurant"}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            // Add CORS headers if needed
+          },
+          credentials: "include", // Important for cookies
+          body: JSON.stringify({
+            email: formData.email,
+            password: formData.password,
+          }),
+        }
+      );
+      if (!res.ok) {
+        const err = await res.json();
+        throw new Error(
+          err.message || (!isRegister ? "Sign up failed" : "Login failed")
+        );
+      }
+      const data = await res.json();
+      console.log("success", data);
+
+      alert("Signup successful!");
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
     }
-    const data = await res.json();
-    console.log("success" , data);
-
-
-    alert("Signup successful!");
-  } catch(err: any){
-    setError(err.message)
-
-  }
-  finally{
-    setLoading((false));
-  }
-}
+  };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -71,82 +72,92 @@ function SignUp() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen  bg-white/10 ">
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-4  w-[341px] h-[568px]  max-sw-sm bg-white/6 p-6  shadow-lg text-center backdrop-blur-md border border-white/10 rounded-2xl blur-in-lg "
-        >
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 text-center">
           <h1 className="text-5xl f font-bold mb-7 mt-[60px]">
-            {!isRegister ? "Sign Up" : "Sign In"}
+            {!isRegister ? "ลงทะเบียน" : "เข้าสู่ระบบ"}
           </h1>
           {error && <h2 className="text-red-400 text-l">{error}</h2>}
+          {!isRegister && (
+            <input
+              type="text"
+              name="name"
+              placeholder="ชื่อ-นามสกุล"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-300 mb-1.5"
+            />
+          )}
           <input
             type="email"
             name="email"
-            placeholder="Your Email"
+            placeholder="อีเมล"
             value={formData.email}
             onChange={handleChange}
             required
             className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-300 mb-1.5"
           />
-
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-300 mb-1.5"
-          />
-          
-          {!isRegister && (
+          <div className="relative w-80">
             <input
-              type="password"
-              name="confirmPassword"
-              placeholder="Confirm Password"
-              value={formData.confirmPassword}
+              type={showPassword ? "text":"password"}
+              name="password"
+              placeholder="รหัสผ่าน"
+              value={formData.password}
               onChange={handleChange}
               required
-              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-300"
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-300 mb-1.5 w-full"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            >
+              {showPassword ?  <Eye size={18} /> : <EyeOff size={18} />}
+            </button>
+          </div>
+
+          {!isRegister && (
+            <input
+              type="tel"
+              name="phone"
+              placeholder="0xx-xxx-xxxx"
+              value={formData.phone}
+              onChange={handleChange}
+              required
+              pattern="[0-9]{10}"
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-300 mb-1.5"
             />
           )}
 
-          {/* <button
-                        type="submit"
-                        className="bg-gray-800 text-white py-3 rounded hover:bg-gray-900 transition-colors font-semibold cursor-pointer text-xl"
-                    >
-                        Create Account
-                    </button>
-                    <Button asChild>
-                        <Link href="/login">Create Account</Link>
-                    </Button> */}
           <Button className="px-6 py-6 text-lg " variant="outline">
-            {!isRegister ? "Create Account" : "Log-In"}
+            {!isRegister ? "สร้างบัญชี" : "ล็อคอิน"}
           </Button>
           {isRegister && (
             <div className="flex justify-between items-center">
               <div className="flex items-center gap-3">
                 <Checkbox id="terms" />
-                <Label htmlFor="terms">Remember me</Label>
+                <Label htmlFor="terms">จำการเข้าสู่ระบบ</Label>
               </div>
               {/* <span className="text-gray-900 hover:underline cursor-pointer text-l ">
                 Forgot Password
               </span> */}
-              <Button variant="link">Forgot Password</Button>
+              <Button variant="link">ลืมรหัสผ่าน</Button>
             </div>
           )}
 
-          <div className="text-gray-700">
+          <div className="text-gray-700 text-center">
             <span>
-              {!isRegister
-                ? "Already have an account? "
-                : "Don't have an account? "}
+              {!isRegister ? "มีบัญชีอยู่แล้ว ? " : "ยังไม่ได้สร้างบัญชี ?"}
             </span>
             <span
               className="text-gray-900 hover:underline cursor-pointer  "
               onClick={() => setIsRegister(!isRegister)}
             >
-               {!isRegister ?  <Button variant="link">Sign in</Button> :  <Button variant="link">Sign up</Button>}
+              {!isRegister ? (
+                <Button variant="link">เข้าสู่ระบบ</Button>
+              ) : (
+                <Button variant="link">ลงทะเบียน</Button>
+              )}
             </span>
           </div>
         </form>
@@ -155,5 +166,4 @@ function SignUp() {
   );
 }
 
-
-export default SignUp;
+export default Auth;
